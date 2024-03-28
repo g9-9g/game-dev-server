f54e91bc59b52a150c5d781f1c812bec08fc2a0b
le minh <leminhnguyen1924@gmail.com>
2024-03-29 01:28:02 +0700

    modified:   .env 	modified:   controllers/userAuth.js 	modified:   middlewares/auth.js

--- controllers/userAuth.js
+++ controllers/userAuth.js
@@ -3,83 +3,73 @@ const db = require("../model");
 const jwt = require("jsonwebtoken");
 
 
-//signing a user up
-//hashing users password before its saved to the database with bcrypt
 const signup = async (req, res) => {
- try {
-   const { username, email, password } = req.body;
-   const data = [username, email, await bcrypt.hash(password, 10)]
-   
-   //saving the user
-   const db_res = await db.query(
+  try {
+    const { username, email, password } = req.body;
+    const data = [username, email, await bcrypt.hash(password, 10)]
+
+    const db_res = await db.query(
     `INSERT 
     INTO "users" (username, email, password)
     VALUES ($1, $2, $3)`, data)
 
-   //if user details is captured
-   //generate token with the user's id and the secretKey in the env file
-   // set cookie with the token generated
-   if (db_res.rowCount > 0) {
-    return await login(req,res)
-   } else {
-     return res.status(409).send("Details are not correct");
-   }
- } catch (error) {
-   console.log(error);
- }
+  if (db_res.rowCount > 0) {
+    return await login(req,res);
+  } else {
+    return res.status(409).send("Details are not correct");
+  }
+  } catch (error) {
+    return res.status(409).json({ error: error })
+  }
 };
 
 
 //login authentication
 
 const login = async (req, res) => {
-    try {
+  try {
     const { username, email, password } = req.body;
 
-   //find a user by their email
-   const db_res = await db.query(
+    const db_res = await db.query(
     `SELECT *
     FROM "users" WHERE email = $1`, [email])
 
     console.log(db_res)
     
-   //if user email is found, compare password with bcrypt
-   if (db_res.rowCount > 0) {
-    var user = db_res.rows[0];
-    console.log(user)
-    const isSame = await bcrypt.compare(password, user.password);
+    if (db_res.rowCount > 0) {
+      var user = db_res.rows[0];
+      const isSame = await bcrypt.compare(password, user.password);
 
-     //if password is the same
-      //generate token with the user's id and the secretKey in the env file
+      if (isSame) {
+        // Only sign username and email (no)
+        let token = jwt.sign( {"user" : { "username" : user.username, "email" : user.email }}, process.env.secretKey, {
+          expiresIn: 1 * 24 * 60 * 60 * 1000,
+        });
 
-     if (isSame) {
-       let token = jwt.sign( user, process.env.secretKey, {
-         expiresIn: 1 * 24 * 60 * 60 * 1000,
-       });
+        res.cookie("user", { "username" : user.username, "email" : user.email }, { 
+          maxAge: 1 * 24 * 60 * 60, 
+          httpOnly: true,
+          // secure: process.env.NODE_ENV === "production"
+        });
 
-       //if password matches wit the one in the database
-       //go ahead and generate a cookie for the user
-       res.cookie("jwt", token, { maxAge: 1 * 24 * 60 * 60, httpOnly: true });
-       res.cookie("user", user, {
-        maxAge: 1 * 24 * 60 * 60, httpOnly: true
-       })
-       console.log("user", JSON.stringify(user, null, 2));
-       console.log(token);
-       //send user data
-       return res.status(201).send(user);
-     } else {
-       return res.status(401).send("Authentication failed");
-     }
-   } else {
-     return res.status(401).send("Authentication failed");
-   }
- } catch (error) {
-   console.log(error);
- }
+        console.log("user", JSON.stringify(user, null, 2));
+        console.log(token);
+        
+        return res.status(201).send(token);
+
+
+      } else {
+        return res.status(401).send("Authentication failed");
+      }
+  } else {
+    return res.status(401).send("Authentication failed");
+  }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const signout = async (req, res, next) => {
-  res.clearCookie('jwt');
   res.clearCookie('user');
   return res.status(200).send("Sign out successfullly")
 }
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,60 +5,46 @@ const jwt = require("jsonwebtoken");
 
 const checkUser = async (req, res, next) => {
     try {
-        console.log(req.body)
-        var name = req.body.username
-        var email = req.body.email
-        var password = req.body.password
-        
-        const nameRegex = /^[a-zA-Z][a-zA-Z0-9_-]{2,19}$/
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d\S]{8,}$/
-        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+        const { username, email } = req.body
 
-        // if (!(nameRegex.test(name) && passwordRegex.test(password) && emailRegex.test(email))) {
-        //     return res.status(409).send("invalid name or password email");
-        // }
-        var db_res = await db.query(`SELECT * FROM "users" WHERE username = $1`, [name])
-        
-        console.log(db_res.rows.length)
-        // if username exist in the database respond with a status of 409
+        var db_res = await db.query(`SELECT * FROM "users" WHERE username = $1`, [username])
+        var db_res2 = await db.query(`SELECT * FROM "users" WHERE email = $1`, [email])
+
+        console.log(db_res)
+
         if (db_res.rows.length > 0) {
-            return res.status(409).send("username already taken");
+            return res.status(409).send("username has already taken");
         }
-        
-        //checking if email already exist
-         db_res2 = await db.query(`SELECT * FROM "users" WHERE email = $1`, [email])
-        console.log(db_res2)
-        //if email exist in the database respond with a status of 409
         if (db_res2.rows.length > 0) {
-            return res.status(409).send("Authentication failed");
+            return res.status(409).send("email has already taken");
         }
     
-    next();
+        return next();
     } catch (error) {
-        console.log(error);
-        return res.status(409).send("dm ong dang")
+        console.log(error)
+        return res.status(409).send("Can not create account")
     }
 }
 
 const checkToken = async (req, res, next) => {
-    console.log(req.body)
-    try {
-        const token = req.body.token || req.query.token || req.headers['x-access-token'];
-        console.log(token)
-        const decodedToken = jwt.verify(token, process.env.secretKey);
-        const user = decodedToken.user;
-        console.log(user);
-        if (req.body.user_id && req.body.user_id !== user.user_id) {
-            throw 'Invalid user ID';
-        } else {
-            next();
-        }
-    } catch(error) {
-        console.log(error)
-        res.status(401).json({
-            error: new Error('Invalid request!')
-        });
+    const token = req.body.token || req.query.token || req.headers['x-access-token'] || req.headers['authorization'];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
     }
+    jwt.verify(token, process.env.secretKey, (err, decoded) => {
+        if (err) {
+            return res.status(401).json({ message: 'Invalid token' });
+        }
+        console.log(JSON.stringify(req.cookies.user), JSON.stringify(decoded.user))
+        // "2-step verify"
+        if (!req.cookies.user || JSON.stringify(req.cookies.user) !== JSON.stringify(decoded.user)) {
+            return res.status(401).json({ message: 'Already signout'});
+        }
+        
+        req.user = decoded.user;
+        next();
+    });
 }
 
 module.exports = {
